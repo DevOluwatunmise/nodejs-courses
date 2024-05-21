0097c5ae53bc48a66833920e6a37b826d88de940
DevOluwatunmise <tunmisedare@gmail.com>
2024-05-21 10:29:31 +0100

    update

--- mongoTut/controllers/logoutController.js
+++ mongoTut/controllers/logoutController.js
@@ -4,7 +4,9 @@
 const usersDB = {
     users: require("../model/users.json"),
     setUsers: function (data) 
-    {this.users = data},
+    {this.users = data;
+
+    },
   };
 
   const fsPromises = require("fs").promises
